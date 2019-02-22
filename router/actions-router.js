2c1ddfaaa0b755aecbd4403851fa06a7491c5de6
KieranVieira <kieranvieira@live.com>
2019-02-22 13:20:47 -0500

    Completed CRUD for actions router

--- router/actions-router.js
+++ router/actions-router.js
@@ -24,4 +24,88 @@ router.post('/', (req, res) => {
     }
 });
 
+router.get('/', (req, res) => {
+    db('actions')
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(error => {
+            res.status(500).json({
+                message:"Server could not get data",
+                error
+            })
+        })
+});
+
+router.get('/:id', (req, res) => {
+    db('actions')
+        .where({ id: req.params.id })
+        .then(action => {
+            if(action){
+               res.status(200).json(action) 
+            }else{
+                res.status(404).json({
+                    message: "Couldn't find action with given ID"
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Server could not retrieve action",
+                error
+            })
+        })
+});
+
+router.put('/:id', (req, res) => {
+    try {
+        db('actions')
+            .where({ id: req.params.id })
+            .update(req.body)
+            .then(count => {
+                if(count){
+                    res.status(200).json({
+                        message: "Action was updated"
+                    })
+                }else{
+                    res.status(404).json({
+                        message: "Action could not be found with given ID"
+                    })
+                }
+            })
+            .catch(error => {
+                res.status(400).json({
+                    message: "Bad request, please provide required fields",
+                    error
+                })
+            })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server could not update",
+            error
+        })
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    db('actions')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+        if(count){
+            res.status(204).end();
+        }else{
+            res.status(404).json({
+                message: "Action with given ID could not be found"
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Server could not delete Action",
+            error
+        })
+    })
+});
+
 module.exports = router;
