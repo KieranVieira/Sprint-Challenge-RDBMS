29f27a824c4f8aa7d8e37eeba7face6671674d4f
KieranVieira <kieranvieira@live.com>
2019-02-22 13:03:31 -0500

    Finished get route for projects and actions

--- router/projects-router.js
+++ router/projects-router.js
@@ -24,4 +24,36 @@ router.post('/', (req, res) => {
     }
 });
 
+router.get('/:id', (req, res) => {
+    db('projects')
+        .where({ id: req.params.id })
+        .first()
+        .then(project => {
+            if(project){
+                db('actions')
+                .where({ project_id: req.params.id })
+                .then(actions => {
+                    project.actions = actions
+                    res.status(200).json(project)
+                })
+                .catch(error =>{
+                    res.status(500).json({
+                        message: "Server could not find actions",
+                        error
+                    })
+                })
+            }else{
+                res.status(404).json({
+                    message: "Could not find project with given ID",
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Server could not retrieve project",
+                error
+            })
+        })
+});
+
 module.exports = router;
