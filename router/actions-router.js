462d073a7851075f7d6a1d4e5ec0d4b34c33763c
KieranVieira <kieranvieira@live.com>
2019-02-22 12:02:39 -0500

    Added postroute for actions

--- router/actions-router.js
+++ router/actions-router.js
@@ -4,7 +4,24 @@ const db = require('../database/dbConfig')
 const router = express.Router();
 
 router.post('/', (req, res) => {
-
+    try {
+        db('actions')
+            .insert(req.body)
+            .then(id => {
+                res.status(201).json(id)
+            })
+            .catch(error => {
+                res.status(400).json({
+                    message:"Bad request, please provide required fields",
+                    error
+                })
+            })
+    } catch (error) {
+        res.status(500).json({
+            message:"Server could not post project",
+            error
+        })
+    }
 });
 
 module.exports = router;
