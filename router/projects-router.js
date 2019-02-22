9d2c1c83f3b7dea7156bf45ccf47917fc8ff429d
KieranVieira <kieranvieira@live.com>
2019-02-22 13:15:56 -0500

    Compled CRUD for Projects router

--- router/projects-router.js
+++ router/projects-router.js
@@ -24,6 +24,19 @@ router.post('/', (req, res) => {
     }
 });
 
+router.get('/', (req, res) => {
+    db('projects')
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(error => {
+            res.status(500).json({
+                message:"Server could not get data",
+                error
+            })
+        })
+});
+
 router.get('/:id', (req, res) => {
     db('projects')
         .where({ id: req.params.id })
@@ -51,6 +64,57 @@ router.get('/:id', (req, res) => {
         .catch(error => {
             res.status(500).json({
                 message: "Server could not retrieve project",
+                error
+            })
+        })
+});
+
+router.put('/:id', (req, res) => {
+    try {
+        db('projects')
+            .where({ id: req.params.id })
+            .update(req.body)
+            .then(count => {
+                if(count){
+                    res.status(200).json({
+                        message: "Project was updated"
+                    })
+                }else{
+                    res.status(404).json({
+                        message: "Projects could not be found with given ID"
+                    })
+                }
+            })
+            .catch(error => {
+                res.status(400).json({
+                    message: "Bad request, please provide required fields",
+                    error
+                })
+            })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server could not update",
+            error
+        })
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    db('projects')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if(count){
+                res.status(204).end();
+            }else{
+                res.status(404).json({
+                    message: "Project with given ID could not be found"
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Server could not delete project",
                 error
             })
         })
