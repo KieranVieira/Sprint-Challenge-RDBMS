f06a22795034a9a17737636a2b472ddc4f1d08e9
KieranVieira <kieranvieira@live.com>
2019-02-22 13:44:32 -0500

    Adjusted migration table to include increments for id

--- database/migrations/20190222133050_context_actions.js
+++ database/migrations/20190222133050_context_actions.js
@@ -1,18 +1,19 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('context_actions', tbl => {
+        tbl.increments();
         tbl.integer('context_id')
             .unsigned()
             .references('id')
             .inTable('contexts')
             .onDelete('CASCADE')
-            .onUpdate('CASCADE')
+            .onUpdate('CASCADE');
         tbl.integer('action_id')
             .unsigned()
             .references('id')
             .inTable('actions')
             .onDelete('CASCADE')
-            .onUpdate('CASCADE')
+            .onUpdate('CASCADE');
     })
 };
 
